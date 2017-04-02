7ba3128f1352532135b1495bd5657767f70a4619
Yamazaki Mitsufumi <mitsufumi.yamazaki@outlook.jp>
2017-04-03 01:46:37 +0900

    add Scripts
    add Player and PlayerController scripts.
    These scripts move Tag from "It" to "Player".
    These scripts are core of this project.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+    public event Action<GameObject,GameObject> onTauch = delegate { };
+    public int x=0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (this.tag.Contains("It"))
+        {
+            if (collision.gameObject.tag.Contains("Player"))
+            {
+                onTauch(this.gameObject, collision.gameObject);
+                x++;
+            }
+        }
+    }
+}
