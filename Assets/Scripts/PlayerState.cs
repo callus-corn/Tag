781a05b469c65f9dbb68d5b312f07e8c0e7c24e7
Yamazaki Mitsufumi <mitsufumi.yamazaki@outlook.jp>
2017-04-11 15:22:35 +0900

    update refactoring
    Change code with UniRx.
    And add PlayerState for observing tauchable.

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class PlayerState : MonoBehaviour
+{
+    public bool isHuman { get; private set; }
+    public bool isIt { get; private set; }
+    public bool isTauchable { get; private set; }
+    Subject<bool> subject = new Subject<bool>();
+
+    void Start()
+    {
+        ToHuman();
+        subject.Delay(TimeSpan.FromSeconds(3))
+               .Subscribe(_ => {
+                   ToIt();
+                   Debug.Log("tauchable");
+               });
+    }
+
+    private void ToIt()
+    {
+        isHuman = false;
+        isIt = true;
+        isTauchable = true;
+    }
+
+    public void ToHuman()
+    {
+        isHuman = true;
+        isIt = false;
+        isTauchable = false;
+    }
+
+    public void ToStay()
+    {
+        isHuman = false;
+        isIt = true;
+        isTauchable = false;
+        subject.OnNext(true);
+    }
+
+}
---
+++ Assets/Scripts/Tauch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tauch : MonoBehaviour
+{
+    public void It(Player taucher,Player tauchee)
+    {
+        if (tauchee.state.isHuman)
+        {
+            tauchee.state.ToStay();
+            taucher.state.ToHuman();
+            Debug.Log("tauch! "+taucher.ToString());
+        }
+    }
+}
